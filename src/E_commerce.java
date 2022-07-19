55d152ab4843b7edb7918b178e63dd8823958026
karanmj <mjkaran333@gmail.com>
2022-07-19 14:21:46 +0530

    Completed

---
+++ src/E_commerce.java
@@ -0,0 +1,26 @@
+
+public class E_commerce {
+	
+public static void main(String[] args) {
+		
+		int k = 175;
+		
+		if (k <=176 && k ==175) {
+			
+			System.out.println(" 101 - Apple - 10");
+			System.out.println(" 201 - Mango - 25");
+			System.out.println(" 301 - brush - 5");
+			System.out.println(" 401 - paste - 20");
+		}
+		
+		else if (k <=174 || k == 333) {
+			System.out.println("Exit");
+		}
+		
+		else {
+			System.out.println("Exit");
+		}
+
+	}
+
+}
